8087f3fd60f97377c6bc87787fb1b2e18813c069
egamberdiyevxushnud <xushnudegamberdiyev0@gmail.com>
2024-04-06 09:23:07 +0500

    update file

---
+++ Angular/Lesson4/project/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { CarComponent } from './companentes/car/car.component';
+
+export const routes: Routes = [
+    {path:'Car', title: 'Car', component: CarComponent}
+];
